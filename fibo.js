3ae0f2ce9574cab273f9a43037d68d0953e2318b
rubyCiide <joelpaul900@gmail.com>
2023-12-10 22:49:38 +0530

    fix fibo bugs, add fiboRecursive

--- fibo.js
+++ fibo.js
@@ -1,13 +1,26 @@
-function fibo(n){
+function fibo(n) {
   let first = 0;
   let second = 1;
-  const arr  = [];
+  let third;
+  const arr = [];
   for(let i = 0;i<n;i++){
     arr.push(first);
+    third = first+second;
     first = second;
-    second = second+first;
+    second = third;
+    third = second;
+    
   }
   return arr;
 }
 
-console.log(fibo(3))
+function fiboRecursive(n) {
+  if (n === 1) return [0, 1]; 
+  if (n === 2) return [0, 1];
+  const prev = fiboRecursive(n - 1);
+  const next = prev[prev.length - 1] + prev[prev.length - 2];
+  return [...prev, next];
+}
+
+console.log(fibo(8)); 
+console.log(fiboRecursive(8)); 
