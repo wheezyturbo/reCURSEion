7b752bea01fc63fa9a8b29ccfb331ceaa683ad2e
rubyCiide <joelpaul900@gmail.com>
2023-12-11 10:44:51 +0530

    fix recursion bug for inputs 0,1 and 2

--- fibo.js
+++ fibo.js
@@ -15,7 +15,8 @@ function fibo(n) {
 }
 
 function fiboRecursive(n) {
-  if (n === 1) return [0, 1]; 
+  if(n<=0) return [];
+  if (n === 1) return [0]; 
   if (n === 2) return [0, 1];
   const prev = fiboRecursive(n - 1);
   const next = prev[prev.length - 1] + prev[prev.length - 2];
@@ -23,4 +24,4 @@ function fiboRecursive(n) {
 }
 
 console.log(fibo(8)); 
-console.log(fiboRecursive(8)); 
+console.log(fiboRecursive(8));
